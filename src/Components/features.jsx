48eb55e4c14197abb6c1b7d3b6b9f58d25a7e8c2
Meanzar <romain.danizel.pro@gmail.com>
2024-03-28 13:27:11 +0100

    fix darkmode button issues

--- src/Components/features.jsx
+++ src/Components/features.jsx
@@ -4,8 +4,8 @@ export default function Features() {
         <div className="grid grid-cols-2  text-center">
             <p className="text-steels border-2 border-gray-200 rounded-md p-2 mr-1 mt-2">Did donald trump was a developer ?</p>
             <p className="text-steels border-2 border-gray-200 rounded-md p-2 ml-1 mt-2">Can we cut a wall with paper</p>
-            <p className="text-steels border-2 border-gray-200 rounded-md p-2 mr-1 mt-2 mb-2">Christophe colomb have more subscriber than Elon musk</p>
-            <p className="text-steels border-2 border-gray-200 rounded-md p-2 ml-1 mt-2 mb-2">Fake News Checker</p>
+            <p className="text-steels border-2 border-gray-200 rounded-md p-2 mr-1 mt-2 mb-2">Christophe colomb have more subscriber than Pewdiepie</p>
+            <p className="text-steels border-2 border-gray-200 rounded-md p-2 ml-1 mt-2 mb-2">Did the third great war has start ?</p>
         </div>
     )
 }
