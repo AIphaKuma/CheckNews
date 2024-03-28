86828d3998fddbf35ca1e23f07a43f0459f5cc8c
Raphael <raphaeltruong27@outlook.com>
2024-03-28 14:51:01 +0100

    Change Font and add separation between chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import MistralClient from "@mistralai/mistralai";
 import Features from "./Components/features";
-import logo from './assets/checknews.png'
+import logo from './assets/checknews.png';
 
 function App() {
   const doChatStream = async function () {
@@ -20,12 +21,15 @@ function App() {
     try {
       const chatStreamResponse = await client.chatStream({
         model: 'open-mistral-7b',
-        messages: [{role: 'user', content: chat}],
+        messages: [{ role: 'user', content: chat }],
         temperature: 0.7, // Ajuste la créativité de la réponse
         top_p: 0.9, // Contrôle la diversité de la réponse en filtrant les tokens les moins probables
         presence_penalty: 0.6, // Encourage des réponses nouvelles et uniques
         frequency_penalty: 0.5, // Réduit la répétition dans la réponse
       });
+
+      // Ajoutez un diviseur ou un espace avant d'afficher la nouvelle réponse
+      document.getElementById("output").innerHTML += "<hr style='margin-top: 20px; margin-bottom: 20px;'/>";
 
       for await (const chunk of chatStreamResponse) {
         if (chunk.choices[0].delta.content !== undefined) {
@@ -40,38 +44,34 @@ function App() {
   };
 
   return (
-  <div className="App flex flex-col items-center min-h-screen bg-white py-8">
-    <div className="container mx-auto px-4 w-full max-w-4xl">
-      <div className="text-center mb-8">
+      <div className="App flex flex-col items-center min-h-screen bg-white py-8">
+        <div className="container mx-auto px-4 w-full max-w-4xl">
+          <label className="label text-black block mb-2 text-xl text-gray-400" htmlFor="question">
+            How can I help you today?
+          </label>
+          <div className="mb-4 flex flex-col md:flex-row">
+            <div className="md:flex-1">
+              <input
+                  className="input w-full px-4 py-2 border-2 bg-white border-gray-300 rounded-md focus:outline-none focus:border-steel text-black"
+                  type="text"
+                  id="chat"
+                  name="question"
+                  placeholder="Entrez votre information"
+              />
+            </div>
+            <div className="md:ml-4 mt-4 md:mt-0">
+              <button className="button bg-steel px-4 py-2 text-white rounded-md" onClick={doChatStream}>
+                Vérifier
+              </button>
+            </div>
+          </div>
+          <Features />
+          <div id="output" className="w-full border-2 border-gray-300 p-4 rounded-md text-steels" style={{ minHeight: '150px' }}>
+          </div>
+          <div id="error" className="message mt-4 bg-red-600 text-white p-4 rounded-md hidden">
+          </div>
+        </div>
       </div>
-    <label className="label text-black block mb-2 text-xl text-gray-400" htmlFor="question">
-        Comment puis-je vous aider ?
-    </label>
-    <div className="mb-4 flex flex-col md:flex-row">
-      <div className="md:flex-1">
-        <input
-          className="input w-full px-4 py-2 border-2 bg-white border-gray-300 rounded-md focus:outline-none focus:border-steel text-black"
-          type="text"
-          id="chat"
-          name="question"
-          placeholder="Entrez votre information"
-        />
-      </div>
-      <div className="md:ml-4 mt-4 md:mt-0">
-        <button className="button bg-steel px-4 py-2 text-white rounded-md" onClick={doChatStream}>
-          Vérifier
-        </button>
-      </div>
-    </div>
-    <Features/>
-    <div id="output" className="w-full border-2 border-gray-300 p-4 rounded-md text-steels" style={{ minHeight: '150px' }}>
-      
-    </div>
-    
-    <div id="error" className="message mt-4 bg-red-600 text-white p-4 rounded-md hidden">
-    </div>
-  </div>
-</div>
   );
 }
 
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="container mx-auto flex justify-center items-center px-4">
           <div className="logo flex justify-center flex-col items-center">
           <img src={logo} alt="Logo checknews" className="w-[150px]" />
-          <h2 className="text-4xl text-steels">Pour une information vérifiée, une société éclairée.</h2>
+          <h2 className="text-4xl text-steels paytone-one-regular">Truth in Every Click</h2>
           </div>
           
         </div>
